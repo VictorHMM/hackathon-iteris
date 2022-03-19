cad45ba1687906d60eb5db12c5a2273b8cb42093
KeythMarianne <79008868+KeythMarianne@users.noreply.github.com>
2022-03-19 13:24:56 -0300

    Esqueleto da página de usuário

---
+++ projeto/src/views/UserCarnival.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <h2>Página do usuário</h2>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'UserCarnival',
+    data() {
+        return {
+            UserCarnival: []
+        }
+    },
+}
+</script>
+
+<style scoped>
+</style>
