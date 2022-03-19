babf424fe728aec5bf63872607e7a0aa117b1318
Victor Medeiros <victorh.mmedeiros@gmail.com>
2022-03-19 10:37:43 -0400

    Esqueleto

--- projeto/src/router/index.js
+++ projeto/src/router/index.js
@@ -1,14 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeCarnival from '../views/HomeCarnival.vue'
+import AboutCarnival from '../views/AboutCarnival.vue'
+import GeneralCarnival from '../views/GeneralCarnival.vue'
+import LocalCarnival from '../views/LocalCarnival.vue'
+import LoginCarnival from '../views/LoginCarnival.vue'
+import NameCarnival from '../views/NameCarnival.vue'
+import SignUpCarnival from '../views/SignUpCarnival.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'HomeCarnival',
+    component: HomeCarnival
+  },
+  {
+    path: '/about',
+    name: 'AboutCarnival',
+    component: AboutCarnival
+  },
+  {
+    path: '/general',
+    name: 'GeneralCarnival',
+    component: GeneralCarnival
+  },
+  {
+    path: '/local',
+    name: 'LocalCarnival',
+    component: LocalCarnival
+  },
+  {
+    path: '/login',
+    name: 'LoginCarnival',
+    component: LoginCarnival
+  },
+  {
+    path: '/name',
+    name: 'NameCarnival',
+    component: NameCarnival
+  },
+  {
+    path: '/signUp',
+    name: 'SignUpCarnival',
+    component: SignUpCarnival
   },
   {
     path: '/about',
