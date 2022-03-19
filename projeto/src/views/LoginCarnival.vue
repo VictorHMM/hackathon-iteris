a74123fe09260e684e9046f3593a23259537fb84
Maria Fernanda <ferdinandamoge@gmail.com>
2022-03-19 16:26:46 -0300

    ok

--- projeto/src/views/LoginCarnival.vue
+++ projeto/src/views/LoginCarnival.vue
@@ -5,17 +5,17 @@
         <h2>Login</h2>
       </div>
 
-<<<<<<< HEAD
-      <v-form>
+      <v-form class="login-box">
         <v-text-field
-          v-model="name_id"
-          label="Seu nome"
+          v-model="email"
+          label="E-mail"
+          placeholder="seuemail@dominio.com"
           required
         ></v-text-field>
         <v-text-field
-          type="email"
-          v-model="email"
-          label="Seu Email"
+          type="password"
+          v-model="password"
+          label="Senha"
           required
         ></v-text-field>
       </v-form>
@@ -29,7 +29,7 @@
             medium
             outlined
             rounded
-            >Login
+            >Entrar
           </v-btn>
         </router-link>
 
@@ -41,32 +41,6 @@
       </div>
     </div>
   </v-app>
-=======
-          <v-form class="login-box">
-              <v-text-field v-model="email" label="E-mail" placeholder="seuemail@dominio.com" required></v-text-field>
-              <v-text-field type="password" v-model="password" label="Senha" required></v-text-field>
-          </v-form>
-
-          <div class="botoes">
-              <router-link to="/user">
-              <v-btn
-              @click="logar"
-              color="green"
-              elevation="2"
-              medium
-              outlined
-              rounded
-              >Entrar
-              </v-btn>
-              </router-link>
-              
-              <router-link to="/signUp">
-              <v-btn color="green" elevation="2" medium outlined rounded>Cadastrar-se</v-btn>
-              </router-link>
-          </div>
-        </div>
-    </v-app>
->>>>>>> f472802e2fd7d123cbef23a43a92c94bc2c02702
 </template>
 
 <script>
