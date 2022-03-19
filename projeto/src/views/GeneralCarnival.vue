fdcb345f01653b7110aed44994001dc6f9c49098
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-19 12:04:28 -0300

    Escolhendo tabela

--- projeto/src/views/GeneralCarnival.vue
+++ projeto/src/views/GeneralCarnival.vue
@@ -1,13 +1,38 @@
 <template>
-    
+  <v-container>
+    <v-card>
+    <v-card-title>
+      Carnival
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="carnival"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
+    name: "GeneralCarnival",
+    data() {
+        return {
+            carnivalList: [],
+        };
+    },
     
-}
+    }
+};
 </script>
 
 <style scoped>
-
 </style>
