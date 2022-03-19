4d08627c3382860ab326939e0c0a1d764de4cbd9
KeythMarianne <79008868+KeythMarianne@users.noreply.github.com>
2022-03-19 13:22:10 -0300

    Criação da página de login do usuário

--- projeto/src/views/LoginCarnival.vue
+++ projeto/src/views/LoginCarnival.vue
@@ -1,13 +1,58 @@
 <template>
-  <div>
-    <h1>udhfuh</h1>
-  </div>
+    <v-app>
+        <div style="width: 80%" class="login">
+          <div class="login-form">
+              <h2>Login</h2>
+          </div>
+
+          <v-form class="login-box">
+              <v-text-field v-model="login_id" label="ID" required></v-text-field>
+              <v-text-field type="password" v-model="login_pass" label="Password" required></v-text-field>
+          </v-form>
+
+          <div class="botoes">
+              <router-link to="/user">
+              <v-btn
+              @click="logar"
+              color="green"
+              elevation="2"
+              medium
+              outlined
+              rounded
+              >Login
+              </v-btn>
+              </router-link>
+              
+              <router-link to="/signUp">
+              <v-btn color="green" elevation="2" medium outlined rounded>Cadastrar-se</v-btn>
+              </router-link>
+          </div>
+        </div>
+    </v-app>
 </template>
 
-scp
 <script>
-export default {};
+export default {
+  name: "LoginCarnival",
+  data() {
+    return {
+      login_id: "",
+      login_pass: "",
+    };
+  },
+};
 </script>
 
-<style scoped>
+<style>
+.login {
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 30px;
+}
+.login-box {
+  max-width: 100%;
+}
+.botoes a {
+  text-decoration: none;
+}
 </style>
